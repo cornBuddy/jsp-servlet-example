fe46413b7920f4b3fb9ffad91bd68c7bfd2be600
Heorhi Sakovich <heorhi.sakovich@gmail.com>
2018-08-05 14:41:58 +0300

    ref(servlet): host to /

--- src/main/java/com/yotam/AllUsersServlet.java
+++ src/main/java/com/yotam/AllUsersServlet.java
@@ -12,7 +12,7 @@ import java.util.List;
 /**
  * Created by yotamm on 28/04/16.
  */
-@WebServlet("/users")
+@WebServlet("/")
 public class AllUsersServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
